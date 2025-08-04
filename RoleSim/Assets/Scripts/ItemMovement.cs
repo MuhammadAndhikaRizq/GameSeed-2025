7c2e0697b6aed86ab4fb662819b0206f6c24f14a
Muhammad Andhika Rizq <mandhikarizq@gmail.com>
2025-08-04 14:17:41 +0700

    Item Movement

---
+++ RoleSim/Assets/Scripts/ItemMovement.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemMovement : MonoBehaviour
+{
+    private Vector3 startPosition;
+    private Vector3 offset;
+
+    public bool isDragging = false;
+
+    void Awake()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        InputSystem();
+        if (isDragging)
+        {
+            transform.position = MouseWorldPosition(Input.mousePosition) + offset;
+        }
+    }
+
+    private void InputSystem()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(worldPos, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                isDragging = true;
+                offset = transform.position - MouseWorldPosition(Input.mousePosition);
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0) && isDragging)
+        {
+            DropObject();
+        }
+    }
+
+    protected virtual void DropObject()
+    {
+        isDragging = false;
+    }
+    
+    protected virtual Vector3 MouseWorldPosition(Vector3 inputPosition)
+    {
+        inputPosition.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(inputPosition);
+    }
+
+    public void ResetPosition()
+    {
+        isDragging = false;
+        transform.position = startPosition;
+    }
+}
